5086762b2167e37c702699cb52909fcce41d2253
Nihat Halici <nihat.halici@gmail.com>
2022-07-15 09:31:34 +0200

    Update hello.py

--- CHPT-03-Installing-Tkinter/hello.py
+++ CHPT-03-Installing-Tkinter/hello.py
@@ -1,3 +1,12 @@
+"""by Mark Roseman
+   mark@markroseman.com
+
+   View this code at
+   https://github.com/roseman/tkdocs/
+   or
+   https://tkdocs.com/
+"""
+
 from tkinter import *
 from tkinter import ttk
 root = Tk()
